33fd50f6f17f6e9ddcaeebd00acc8110f6f55040
kudave20 <dave1315@korea.ac.kr>
2021-07-09 17:35:58 +0900

    큐브 회전 수정

--- Assets/Scripts/CubeController.cs
+++ Assets/Scripts/CubeController.cs
@@ -4,34 +4,111 @@ using UnityEngine;
 
 public class CubeController : MonoBehaviour
 {
+    [SerializeField]
+    private float speed;
+    private float timer;
+
     public void Up()
     {
-        GameManager.instance.stage.cubes.transform.rotation *= Quaternion.Euler(90, 0, 0);
-        print("HI");
+        timer = 0f;
+        StartCoroutine(UpCoroutine());
+    }
+
+    IEnumerator UpCoroutine()
+    {
+        while (timer * speed < 1f)
+        {
+            timer += Time.deltaTime;
+            GameManager.instance.stage.cubes.transform.rotation *= Quaternion.Euler(90 * Time.deltaTime * speed, 0, 0);
+            yield return null;
+        }
+        GameManager.instance.stage.cubes.transform.rotation *= Quaternion.Euler(90 - 90 * timer * speed, 0, 0);
     }
 
     public void Down()
     {
-        GameManager.instance.stage.cubes.transform.rotation *= Quaternion.Euler(-90, 0, 0);
+        timer = 0f;
+        StartCoroutine(DownCoroutine());
+    }
+
+    IEnumerator DownCoroutine()
+    {
+        while (timer * speed < 1f)
+        {
+            timer += Time.deltaTime;
+            GameManager.instance.stage.cubes.transform.rotation *= Quaternion.Euler(-90 * Time.deltaTime * speed, 0, 0);
+            yield return null;
+        }
+        GameManager.instance.stage.cubes.transform.rotation *= Quaternion.Euler(90 * timer * speed - 90, 0, 0);
     }
 
     public void Left()
     {
-        GameManager.instance.stage.cubes.transform.rotation *= Quaternion.Euler(0, 90, 0);
+        timer = 0f;
+        StartCoroutine(LeftCoroutine());
+    }
+
+    IEnumerator LeftCoroutine()
+    {
+        while (timer * speed < 1f)
+        {
+            timer += Time.deltaTime;
+            GameManager.instance.stage.cubes.transform.rotation *= Quaternion.Euler(0, 90 * Time.deltaTime * speed, 0);
+            yield return null;
+        }
+        GameManager.instance.stage.cubes.transform.rotation *= Quaternion.Euler(0, 90 - 90 * timer * speed, 0);
     }
 
     public void Right()
     {
-        GameManager.instance.stage.cubes.transform.rotation *= Quaternion.Euler(0, -90, 0);
+        timer = 0f;
+        StartCoroutine(RightCoroutine());
     }
+
+    IEnumerator RightCoroutine()
+    {
+        while (timer * speed < 1f)
+        {
+            timer += Time.deltaTime;
+            GameManager.instance.stage.cubes.transform.rotation *= Quaternion.Euler(0, -90 * Time.deltaTime * speed, 0);
+            yield return null;
+        }
+        GameManager.instance.stage.cubes.transform.rotation *= Quaternion.Euler(0, 90 * timer * speed - 90, 0);
+    }
+
+
 
     public void Clock()
     {
-        GameManager.instance.stage.cubes.transform.rotation *= Quaternion.Euler(0, 0, -90);
+        timer = 0f;
+        StartCoroutine(ClockCoroutine());
+    }
+
+    IEnumerator ClockCoroutine()
+    {
+        while (timer * speed < 1f)
+        {
+            timer += Time.deltaTime;
+            GameManager.instance.stage.cubes.transform.rotation *= Quaternion.Euler(0, 0, -90 * Time.deltaTime * speed);
+            yield return null;
+        }
+        GameManager.instance.stage.cubes.transform.rotation *= Quaternion.Euler(0, 0, 90 * timer * speed - 90);
     }
 
     public void CounterClock()
     {
-        GameManager.instance.stage.cubes.transform.rotation *= Quaternion.Euler(0, 0, 90);
+        timer = 0f;
+        StartCoroutine(CounterClockCoroutine());
+    }
+
+    IEnumerator CounterClockCoroutine()
+    {
+        while (timer * speed < 1f)
+        {
+            timer += Time.deltaTime;
+            GameManager.instance.stage.cubes.transform.rotation *= Quaternion.Euler(0, 0, 90 * Time.deltaTime * speed);
+            yield return null;
+        }
+        GameManager.instance.stage.cubes.transform.rotation *= Quaternion.Euler(0, 0, 90 - 90 * timer * speed);
     }
 }
