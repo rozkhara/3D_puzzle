b330a56d8a20a71a5f095621797dd7c488d97577
Rozkhara <nytemute@gmail.com>
2022-03-03 22:13:25 +0900

    implement stage unlock

--- Assets/Scripts/StageSelect.cs
+++ Assets/Scripts/StageSelect.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class StageSelect : MonoBehaviour
 {
+    int highscore_medium, highscore_easy;
     public void SelectEasy()
     {
         SceneManager.LoadScene("EasyScene");
@@ -12,12 +13,26 @@ public class StageSelect : MonoBehaviour
 
     public void SelectMedium()
     {
-        SceneManager.LoadScene("MediumScene");
+        if (PlayerPrefs.HasKey("highscoreeasy"))
+        {
+            highscore_easy = PlayerPrefs.GetInt("highscoreeasy");
+            if (highscore_easy >= 50)
+            {
+                SceneManager.LoadScene("MediumScene");
+            }
+        }
     }
 
     public void SelectHard()
     {
-        SceneManager.LoadScene("HardScene");
+        if (PlayerPrefs.HasKey("highscoremedium"))
+        {
+            highscore_medium = PlayerPrefs.GetInt("highscoremedium");
+            if (highscore_medium >= 50)
+            {
+                SceneManager.LoadScene("HardScene");
+            }
+        }
     }
 
     public void ExitSelectScene()
---
+++ Assets/Scripts/UnlockLevels.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnlockLevels : MonoBehaviour
+{
+    int highscore_medium, highscore_easy;
+    GameObject medium, hard;
+
+    private void Awake()
+    {
+        medium = GameObject.Find("Medium");
+        hard = GameObject.Find("Hard");
+        if (PlayerPrefs.HasKey("highscoreeasy"))
+        {
+            highscore_easy = PlayerPrefs.GetInt("highscoreeasy");
+        }
+        if (PlayerPrefs.HasKey("highscoremedium"))
+        {
+            highscore_medium = PlayerPrefs.GetInt("highscoremedium");
+        }
+    }
+    private void Start()
+    {
+        if (highscore_easy >= 50)
+        {
+            medium.GetComponent<Image>().color = new Color(238 / 255f, 242 / 255f, 121 / 255f);
+        }
+        else
+        {
+            medium.GetComponent<Image>().color = new Color(123 / 255f, 125 / 255f, 65 / 255f);
+        }
+        if (highscore_medium >= 50)
+        {
+            hard.GetComponent<Image>().color = new Color(238 / 255f, 242 / 255f, 121 / 255f);
+        }
+        else
+        {
+            hard.GetComponent<Image>().color = new Color(123 / 255f, 125 / 255f, 65 / 255f);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
