81d91adf797000775e84a3db87c796d51861e162
Rozkhara <nytemute@gmail.com>
2022-03-03 22:15:00 +0900

    revert swipe

--- Assets/Scripts/Swipe.cs
+++ Assets/Scripts/Swipe.cs
@@ -32,18 +32,13 @@ public class Swipe : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (SceneManager.GetActiveScene().name == "TutorialScene")
-        {
-            rotationCheck = false;
-            moveCheck = false;
-        }
+
         if (Input.GetMouseButtonUp(0) && !(GameManager.instance.isFrozen) && !(GameManager.instance.isGameOver))
         {
             //dragging = false;
             if (Mathf.Abs(x) < 0.09f && Mathf.Abs(y) < 0.09f)
             {
                 cubeController.CounterClock();
-                if (SceneManager.GetActiveScene().name == "TutorialScene") rotationCheck = true;
             }
             else if (x != 0f || y != 0f)
             {
@@ -53,13 +48,10 @@ public class Swipe : MonoBehaviour
                     if (x > 0)
                     {
                         cubeController.Right();
-                        if (SceneManager.GetActiveScene().name == "TutorialScene") moveCheck = true;
                     }
                     else
                     {
                         cubeController.Left();
-                        if (SceneManager.GetActiveScene().name == "TutorialScene") moveCheck = true;
-
                     }
                 }
                 else
@@ -67,13 +59,10 @@ public class Swipe : MonoBehaviour
                     if (y > 0)
                     {
                         cubeController.Up();
-                        if (SceneManager.GetActiveScene().name == "TutorialScene") moveCheck = true;
-
                     }
                     else
                     {
                         cubeController.Down();
-                        if (SceneManager.GetActiveScene().name == "TutorialScene") moveCheck = true;
                     }
                 }
             }
@@ -83,8 +72,6 @@ public class Swipe : MonoBehaviour
         if (Input.GetMouseButtonUp(1) && !(GameManager.instance.isFrozen) && !(GameManager.instance.isGameOver))
         {
             cubeController.Clock();
-            if (SceneManager.GetActiveScene().name == "TutorialScene") rotationCheck = true;
-
         }
     }
 
