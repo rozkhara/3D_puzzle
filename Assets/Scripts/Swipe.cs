f0df00d7bbe0e8daab0acb654ef48e61634e9745
Rozkhara <nytemute@gmail.com>
2022-05-10 00:08:12 +0900

    Change logic for rotation

--- Assets/Scripts/Swipe.cs
+++ Assets/Scripts/Swipe.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Swipe : MonoBehaviour
@@ -11,6 +9,10 @@ public class Swipe : MonoBehaviour
     CubeController cubeController;
     Rigidbody rb;
     float x, y;
+    Vector2 initVector, endVector;
+    float theta;
+    float thetaThreshold = 45.0f;
+    Vector3 screenMidPoint;
     public bool rotationCheck, moveCheck;
 
     private bool isButtonPressed;
@@ -20,28 +22,47 @@ public class Swipe : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         cubeController = GameManager.instance.cube;
+        screenMidPoint= GameObject.Find("Canvas").GetComponent<Canvas>().gameObject.transform.position;
     }
-
-    //private void OnMouseDrag()
-    //{
-    //    dragging = true;
-    //}
-
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            initVector = Input.mousePosition - screenMidPoint;
+            Debug.Log("initPos is " + initVector);
+        }
+
         if (Input.GetMouseButtonUp(0) && !(GameManager.instance.isFrozen) && !(GameManager.instance.isGameOver))
         {
+            endVector = Input.mousePosition - screenMidPoint;
+            Debug.Log("endPos is " + endVector);
+            theta = Vector2.SignedAngle(initVector, endVector);
+            Debug.Log("Theta is " + theta);
+
             //dragging = false;
             if (Mathf.Abs(x) < 0.09f && Mathf.Abs(y) < 0.09f)
             {
-                cubeController.CounterClock();
-                PlayRandomSound();
+                //cubeController.CounterClock();
+                //PlayRandomSound();
             }
             else if (x != 0f || y != 0f)
             {
-                if (Mathf.Abs(x) > Mathf.Abs(y))
+                if (Mathf.Abs(theta) > thetaThreshold && Mathf.Abs(theta) < 180.0f - thetaThreshold)
+                {
+                    if (theta > 0.0f)
+                    {
+                        cubeController.CounterClock();
+                        PlayRandomSound();
+                    }
+                    else
+                    {
+                        cubeController.Clock();
+                        PlayRandomSound();
+                    }
+                }
+                else if (Mathf.Abs(x) > Mathf.Abs(y))
                 {
                     if (x > 0)
                     {
@@ -70,12 +91,15 @@ public class Swipe : MonoBehaviour
             }
             x = 0f;
             y = 0f;
+            initVector = new Vector2(0, 0);
+            endVector = new Vector2(0, 0);
+            theta = 0.0f;
         }
-        if (Input.GetMouseButtonUp(1) && !(GameManager.instance.isFrozen) && !(GameManager.instance.isGameOver))
-        {
-            cubeController.Clock();
-            PlayRandomSound();
-        }
+        //if (Input.GetMouseButtonUp(1) && !(GameManager.instance.isFrozen) && !(GameManager.instance.isGameOver))
+        //{
+        //    cubeController.Clock();
+        //    PlayRandomSound();
+        //}
     }
 
     private void FixedUpdate()
