4aa992ddadc6580aa3d0a7572381d15e1d5b180d
DY <dave1315@korea.ac.kr>
2022-07-05 16:02:22 +0900

    배경 큐브 비 회전 추가

--- Assets/Scripts/BackGroundCube.cs
+++ Assets/Scripts/BackGroundCube.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class BackGroundCube : MonoBehaviour
 {
+    private void Update()
+    {
+        transform.Rotate(Vector3.up * 90 * Time.deltaTime, Space.World);
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Floor")
