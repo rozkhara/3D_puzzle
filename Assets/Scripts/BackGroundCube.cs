d7e3e311b7c8d275d3180581caf2c379752136af
DY <dave1315@korea.ac.kr>
2022-07-05 16:10:03 +0900

    배경 큐브 비 씬 변경 시 자연스럽게

--- Assets/Scripts/BackGroundCube.cs
+++ Assets/Scripts/BackGroundCube.cs
@@ -1,12 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BackGroundCube : MonoBehaviour
 {
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
     private void Update()
     {
         transform.Rotate(Vector3.up * 90 * Time.deltaTime, Space.World);
+
+        if (SceneManager.GetActiveScene().name != "MainMenu" && SceneManager.GetActiveScene().name != "SelectScene")
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
